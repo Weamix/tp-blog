af24e6a6f0618e924b9edf9810b29f2b9c88f966
Weamix <weamix16@gmail.com>
2019-10-20 16:21:02 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@
 //database credentials
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'blog2');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_USER', 'my_user');
+define('DB_PASS', 'my_password');
 
 $db = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
